1e1ce4b6988188234dd2303dd8897c5717bacd79
slacker87 <racethesunlive@yahoo.com>
2015-01-17 05:29:00 +0000

    add puma and clean up some non used gems

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass'
 #for nice icons
 gem 'font-awesome-rails'
-
+#for mikes shitty dev box fix
+gem "puma"
 gem 'autoprefixer-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -36,10 +37,6 @@ gem 'redditkit'
 # gem 'unicorn'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
   gem 'quiet_assets' # Turns off the Rails asset pipeline log
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
 end
 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redditgraphed::Application.config.secret_key_base = 'a3969cb19c1f42043cdb897bc7ddf592298c7862c58d8e0a009fe9c853cf71c5970f437af8c738b7f646247ff692e1e4639a25fa2336b7aece3e94cdec5fd3b4'
