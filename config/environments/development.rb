2c42ba9c65fec049e665c262e3ecf30b804bf327
slacker87 <racethesunlive@yahoo.com>
2015-01-17 03:40:12 +0000

    add quiet assests to dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.quiet_assets = true #tell assets to stfu
 end
