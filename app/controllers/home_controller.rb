02ad637cb100c8129715ccbda4682233aa2740ae
slacker87 <racethesunlive@yahoo.com>
2015-01-19 16:15:36 +0000

    make a fancy line graph work with the top ten posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   def index
+    #get the top 10 posts on the front page in the format of {title: upvotes}
+    @subscriber = Subscriber.title_score_hash
+    #sort by upvote count (the value of hash)
+    @subscribersorted = Hash[@subscriber.sort_by{|k, v| v}.reverse]
   end
 
   def new
+    #get the top 10 posts on the front page in the format of {title: upvotes}
     @subscriber = Subscriber.title_score_hash
   end
 end
