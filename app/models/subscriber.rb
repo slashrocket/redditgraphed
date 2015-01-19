909e94460de0470010fdb22e138f04ea641bebad
Sean Oldham <seanosaur@me.com>
2015-01-19 16:18:25 -0600

    Revert "rewrite save_top_ten to pull from top_ten"
    This reverts commit 311272dda2d01502b050e226d1aff59ee6af8a77.

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,16 +3,15 @@ class Subscriber < ActiveRecord::Base
   def self.top_ten
     @front = RedditKit.front_page(options = {:limit => 10})
   end
-  # Saves top ten title, score, subreddit to db
+
   def self.save_top_ten
-    top_ten.each do |f|
-      @new_sub = Subscriber.new
-      @new_sub[:title] = f.title
-      @new_sub[:count] = f.score
-      @new_sub[:subreddit] = f.subreddit
-      @new_sub.save!
+    @subscriber = Subscriber.title_score_hash
+    @subscriber.each_pair do |x,y|
+      new_sub = Subscriber.new
+      new_sub[:title] = x
+      new_sub[:count] = y
+      new_sub.save!
     end
-    return @new_sub
   end
   # Creates hash with title as key and score as value
   def self.title_score_hash
