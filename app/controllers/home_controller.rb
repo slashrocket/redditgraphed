8cfe6ccb667a6baf7bca8dbf5092e01f7a639bca
Sean Oldham <seanosaur@me.com>
2015-01-20 14:18:22 -0600

    remove all traces of home/new
    home/new was just a test. testing is over now, boys!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,5 @@ class HomeController < ApplicationController
     @subscriber = Subscriber.title_score_hash
     #sort by upvote count (the value of hash)
     @subscribersorted = Hash[@subscriber.sort_by{|k, v| v}.reverse]
-    # ONLY USE FOR TESTING THE 'SAVE TO DB' FEATURE
-    @sub2 = Subscriber.save_top_ten
-  end
-
-  def new
-    #get the top 10 posts on the front page in the format of {title: upvotes}
-    @subscriber = Subscriber.title_score_hash
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :home
   get 'home/index'
   root 'home#index'
 end
