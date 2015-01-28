14729d9a76cd205e30d30362f58d8165f32f7ad7
slacker87 <racethesunlive@yahoo.com>
2015-01-28 01:15:15 +0000

    remove junk and add comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,12 +11,11 @@ class HomeController < ApplicationController
   def timeframe
     #get the timeframe we'll be using in minutes from the url
     @time_in_minutes = params[:time]
-    #timeframes allowed in current dropdown list
-    @timeframe = {'now' => 0, '5 minutes' => 5, '30 minutes' => 30, '1 hour' => 60, '6 hours' => 360, '12 hours' => 720, '24 hours' => 1440}
     # Get the top 10 posts on the front page in the format of {title: upvotes} for this timeframe
     @subscriber = Subscriber.title_score_hash
     # Sort by upvote count (the value of hash)
     @subscribersorted = Hash[@subscriber.sort_by{|k, v| v}.reverse]
+    #render the new results on the page
     render partial: 'home/timeframe.js.erb'
   end
 end
