57038c5c5dacaaa0007be4042b589e4014a9bd4d
slacker87 <racethesunlive@yahoo.com>
2015-01-28 01:01:31 +0000

    finish more dropdown framework

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,17 @@ class HomeController < ApplicationController
     # Sort by upvote count (the value of hash)
     @subscribersorted = Hash[@subscriber.sort_by{|k, v| v}.reverse]
     #timeframes allowed in current dropdown list
-    @timeframe = {'now': 0, '5 minutes': 5, '30 minutes': 30, '1 hour': 60, '6 hours': 360, '12 hours': 720, '24 hours': 1440]
+    @timeframe = {'now' => 0, '5 minutes' => 5, '30 minutes' => 30, '1 hour' => 60, '6 hours' => 360, '12 hours' => 720, '24 hours' => 1440}
   end
 
   def timeframe
+    #get the timeframe we'll be using in minutes from the url
+    time_in_minutes = params[:time]
+    #timeframes allowed in current dropdown list
+    @timeframe = {'now' => 0, '5 minutes' => 5, '30 minutes' => 30, '1 hour' => 60, '6 hours' => 360, '12 hours' => 720, '24 hours' => 1440}
+    # Get the top 10 posts on the front page in the format of {title: upvotes} for this timeframe
+    @subscriber = Subscriber.title_score_hash
+    # Sort by upvote count (the value of hash)
+    @subscribersorted = Hash[@subscriber.sort_by{|k, v| v}.reverse]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
-  match 'home/timeframe/:time', 'home#timeframe', via: 'get', as: 'timeframe'
+  match 'home/timeframe/:time', to: 'home#timeframe', via: 'get', as: 'timeframe'
 end
