533bfdacacd9dc8af8df75a9a730b07fb9a4115f
Sean Oldham <seanosaur@me.com>
2015-02-04 16:38:34 -0600

    change from seconds back to days

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -79,13 +79,12 @@ class Subscriber < ActiveRecord::Base
     subreddits = all_posts_unique.map(&:subreddit)
     # Count the number of similar subreddits
     results = {}
-    days = 604800
     while days >= 0 do
-      iteration_days = Time.now.utc - days
+      iteration_days = Time.now.utc - days.days
       todaysposts = all_posts.where('created_at < ? AND created_at > ?', iteration_days, iteration_days - 1.days)
       todayspostsunique = todaysposts.to_a.uniq{ |item| item.title } # Only get unique posts
       results[days] = todayspostsunique.count
-      days -= 86400
+      days -= 1
     end
     return results
   end
