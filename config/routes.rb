46f61775300d1f8deb4069a866038b8cd63801a3
slacker87 <racethesunlive@yahoo.com>
2015-02-10 20:32:04 +0000

    add a search bar to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  ##### SEARCH PATHS
   get 'search/index'
-
   get 'search/results'
+  match '/search', to: 'search#searching', via: 'post', as: 'searching'
 
+  ##### HOME PATHS
   get 'home/index'
   root 'home#index'
   match '/timeframe', to: 'home#timeframe', via: 'post', as: 'timeframe'
   match '/title', to: 'home#title', via: 'post', as: 'title'
+
+  ##### USER PATHS
 end
