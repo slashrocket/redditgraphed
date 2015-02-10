5649f0c33eee17c08d2e144e17f13d4cd31ce083
slacker87 <racethesunlive@yahoo.com>
2015-02-10 20:17:36 +0000

    generate views/controllers for devise user pages and search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,7 @@
+class SearchController < ApplicationController
+  def index
+  end
+
+  def results
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'search/index'
+
+  get 'search/results'
+
   get 'home/index'
   root 'home#index'
   match '/timeframe', to: 'home#timeframe', via: 'post', as: 'timeframe'
