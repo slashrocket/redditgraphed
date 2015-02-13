ef8de4fb909a975549eef0e4e73bb225c3252bd3
slacker87 <racethesunlive@yahoo.com>
2015-02-13 04:48:16 +0000

    fix devise properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-      def after_sign_in_path_for(user)
-        dashboard_url
-    end
+  def after_sign_in_path_for(user)
+    dashboard_url
+  end
+
+  def after_sign_up_path_for(user)
+    dashboard_url
+  end
 end
