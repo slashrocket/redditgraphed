3706e144848ebbf413695524643775fac9d81970
Sean Oldham <seanosaur@me.com>
2015-02-12 20:36:09 -0600

    sign in/out work, flash messages...not so much

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,19 +2,29 @@ class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    # super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    # super
+    # user = sign_in(params[:email], params[:password])
+    if user_signed_in?
+      flash[:success] = "Logged in"
+      redirect_to root_url
+    else
+      flash[:warning] = "Email or password was invalid"
+      redirect_to login_url
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    sign_out
+    flash[:success] = "Logged out"
+    redirect_to root_url
+  end
 
   # The path used after sign in.
   def after_sign_in_path_for(resource)
