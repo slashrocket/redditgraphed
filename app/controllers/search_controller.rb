5833e421046c3ef19d37c984911ea116e799f44e
slacker87 <racethesunlive@yahoo.com>
2015-02-17 11:37:15 +0000

    improve search results from searchkick slightly, also do something if search is blank

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,20 @@
 class SearchController < ApplicationController
   require 'will_paginate/array'
-  
+
   def index
   end
 
   def results
-    @searchedfor = params[:search][:text]
-    @resultsfound = Subscriber.search @searchedfor.html_safe, limit: 100
-    @resultsfoundunique = @resultsfound.to_a.uniq{ |item| item.title }
+    @searchedfor = params[:search][:text].html_safe
+    @resultsfound = Subscriber.search(@searchedfor,
+                         limit: 25,
+                         partial: true,
+                         misspellings: {distance: 2},
+                         fields: ['title^20',
+                                  'subreddit^3',
+                                  'author^10'])
+    if @searchedfor.empty?
+      @resultsfound  = Subscriber.search('*', limit: 25)
+    end
   end
 end
