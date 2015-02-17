e312ab5ba4c2d3e29c95ee0b428f09297fec126b
slacker87 <racethesunlive@yahoo.com>
2015-02-17 22:53:52 +0000

    fix registration issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,12 +3,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_filter :authenticate_scope!, only: [:edit, :update, :destroy]
 
   private
-
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password)
-  end
-
-  def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_conf, :current_password)
+    # my custom fields are :name, :heard_how
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
