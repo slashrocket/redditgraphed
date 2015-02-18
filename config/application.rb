eafea615f93dae0aafd35bcad52be816dc8aeefb
slacker87 <racethesunlive@yahoo.com>
2015-02-18 17:57:55 +0000

    revert

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Redditgraphed
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     #config.after_initialize do
     #  Subscriber.reindex
     #end
