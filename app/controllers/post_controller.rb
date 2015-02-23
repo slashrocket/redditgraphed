3ab9c0a12032e59ef5eee851192f69006f876b11
slacker87 <racethesunlive@yahoo.com>
2015-02-23 16:31:38 +0000

    initial graph one working

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,6 +3,7 @@ class PostController < ApplicationController
     @post = Subscriber.find(params[:title])
     @scrapes = @post.scores
     @timeframe = {'5 minutes' => 5, '10 minutes' => 10, '15 minutes' => 15, '30 minutes' => 30, '45 minutes' => 45, 'hour' => 60}
+    @chartone = Subscriber.pastminutes(@post, 10)
   end
 
   def timeframe
