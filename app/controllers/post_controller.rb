9fb65c4e67b29407cf502f8041eb42edcca66ecf
slacker87 <racethesunlive@yahoo.com>
2015-02-23 16:25:29 +0000

    start on controller and html for details graph one

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,6 +2,10 @@ class PostController < ApplicationController
   def show
     @post = Subscriber.find(params[:title])
     @scrapes = @post.scores
-    
+    @timeframe = {'5 minutes' => 5, '10 minutes' => 10, '15 minutes' => 15, '30 minutes' => 30, '45 minutes' => 45, 'hour' => 60}
+  end
+
+  def timeframe
+    @post = Subscriber.find(params[:title])
   end
 end
