5aa1cbe95bac312f53a1ff7d6956e81d726e53bf
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-05-01 22:20:33 -0500

    Refactor Get Top Ten job

--- app/jobs/get_top_ten_job.rb
+++ app/jobs/get_top_ten_job.rb
@@ -1,10 +1,10 @@
 class GetTopTenJob < ActiveJob::Base
-  after_perform do |job|
+  after_perform do
     GetTopTenJob.set(wait: 5.minute).perform_later
   end
   queue_as :default
 
-  def perform(*args)
+  def perform(*)
     Subscriber.save_top_ten
   end
 end
