360ce1c5c1785eec566d397e494bc3decef06f27
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-05-01 22:11:22 -0500

    Refactor Search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,9 @@ class SearchController < ApplicationController
   def results
     @searchedfor = params[:search][:text].html_safe
     @resultsfound = Subscriber.search(@searchedfor,
-                         limit: 25,
-                         misspellings: {distance: 1},
-                         fields: ['title^20'])
-    if @searchedfor.empty?
-      @resultsfound  = Subscriber.search('*', limit: 25)
-    end
+                                      limit: 25,
+                                      misspellings: { distance: 1 },
+                                      fields: ['title^20'])
+    @resultsfound  = Subscriber.search('*', limit: 25) if @searchedfor.empty?
   end
 end
