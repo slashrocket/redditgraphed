6cac2c1cade27ff8ba4f71364ba9eb02f71af6fe
askl56 <amscott@openmailbox.org>
2015-05-21 19:16:57 +0100

    begun setting up testing environment

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,16 +1,1 @@
 require 'rails_helper'
-
-# dummy spec to have some skeleton, not real tests
-describe PostController do
-  let(:post_controller) do
-    PostController.new
-  end
-
-  it '#show' do
-    expect(post_controller.show).to eq('Exception in RSpec')
-  end
-
-  it '#timeframe' do
-    expect(post_controller.timeframe).to eq('Exception in RSpec')
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-# dummy spec to have some skeleton, not real tests
 describe User do
   let(:user) do
     User.new
   end
 
   it '#slug' do
-    expect(user.slug).to eq(nil)
+    expect(user.slug).to eq('Fill this in by hand')
   end
 
   it '#to_param' do
-    expect(user.to_param).to eq(nil)
+    expect(user.to_param).to eq('Fill this in by hand')
   end
 end
